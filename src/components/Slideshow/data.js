38e1cae40614eadc4b1b9009fe56654266153ae3
gshrmafp <gsharmafp@gmail.com>
2024-11-08 22:29:59 +0530

    First Commit with code

---
+++ src/components/Slideshow/data.js
@@ -0,0 +1,5 @@
+export const slideshowText ={
+    subheadering1: 'What UDO Do For You',
+    heading:'Digital Marketing Services We Provide best service',
+    subheading:'At UDO Digitals, we’re all about using the power of the internet to help businesses grow! We love what we do, and we’ve been doing it for a long time in a digital marketing field.',
+}
